b56040a9a5e4f21e93f17d679fb794e2b077a611
Akira <ruiakirab@gmail.com>
2022-05-01 18:17:13 +0900

    DB接続

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:16.15
 
 WORKDIR /code
 
-COPY ./api/*package*.json /code/
+COPY ./api/package*.json /code/
 
 RUN npm install -g npm@8.8.0
 
--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
